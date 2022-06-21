e16b558916489625bb8976c00225dfd3c0465280
hamitovdaniil <hamitovdaniil6@gmail.com>
2022-06-21 15:12:58 +0500

    add slayder

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-function openTab(evt, tabName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-	  tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-	  tablinks[i].className = tablinks[i].className.replace(" tabs__active", "");
-	}
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " tabs__active";
-}
+const boxs = document.querySelectorAll('.tabcontent')
+const btns = document.querySelectorAll('.tablinks')
+
+for (let index = 0; index < btns.length; index++) {
+    const btn = btns[index]
+    const box = boxs[index]
+
+    btn.addEventListener('click', (e) => {
+		console.log(btn); 
+	 
+        boxs.forEach((element) => {
+            element.classList.remove('active')
+        })
+        btns.forEach((element) => {
+            element.classList.remove('active')
+        })
+        btn.classList.add('active')
+        box.classList.add('active')
+    })
+};
+
